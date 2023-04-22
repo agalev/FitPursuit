db3494f7887f7f30c4871e96a6ecce97b41772cb
Alexander <alex.galev@yahoo.com>
2023-04-22 13:14:35 -0600

    initial commit with gitignore, .env, Pipfile, server/config.py

---
+++ server/config.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+api = Api(app)
+
+bcrypt = Bcrypt(app)
+
+CORS(app)
