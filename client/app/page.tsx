bb0f66dc14d7e4af6ffb3fc86a02d6ef611a4d75
Alexander <alex.galev@yahoo.com>
2023-04-24 13:40:02 -0600

    tw-elements

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,33 @@
+'use client'
+import { useEffect } from 'react'
+import { Input, Ripple, initTE } from 'tw-elements'
+
 export default function Home() {
-  return (
-    <main>
-      <h1>FitPursuit</h1>
-    </main>
-  )
+	useEffect(() => {
+		initTE({ Ripple, Input })
+	}, [])
+	return (
+		<main>
+			<h1>FitPursuit</h1>
+			<button
+				type='button'
+				data-te-ripple-init
+				data-te-ripple-color='light'
+				className='inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]'
+			>
+				Click me
+			</button>
+			<div className='relative mb-3' data-te-input-wrapper-init>
+				<input
+					type='text'
+					className='peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
+					id='exampleFormControlInput1'
+					placeholder='Example label'
+				/>
+				<label className='pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary'>
+					Example label
+				</label>
+			</div>
+		</main>
+	)
 }
