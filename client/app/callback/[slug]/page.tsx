3f8a43fc79b8ce0c6ca7d312aa469064ffb81d08
Alexander <alex.galev@yahoo.com>
2023-04-27 14:43:52 -0600

    fetching *ALL* activities for logged user through strava and set up proxy to connect with Flask app

--- client/app/callback/[slug]/page.tsx
+++ client/app/callback/[slug]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useEffect } from 'react'
+import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 
 export default function Callback() {
@@ -8,7 +9,10 @@ export default function Callback() {
 	const auth_code = query.toString().match(auth_code_regex)
 	// const scope_regex = /(?<=scope=).*/
 	// const scope = query.toString().match(scope_regex)
-	let tokens = {}
+	let auth_tokens = {}
+	let activities = []
+	let page = 1
+
 	useEffect(() => {
 		fetch(
 			`https://www.strava.com/oauth/token?client_id=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_SECRET}&code=${auth_code}&grant_type=authorization_code`,
@@ -18,12 +22,42 @@ export default function Callback() {
 		)
 			.then((res) => res.json())
 			// .then((data) => console.log(data))
-			.then((data) => tokens = data)
-			.then(() => console.log(tokens))
+			.then(
+				(data) =>
+					(auth_tokens = {
+						access_token: data.access_token,
+						refresh_token: data.refresh_token
+					})
+			)
 	}, [])
+
+	const getActivities = () => {
+		fetch(
+			`https://www.strava.com/api/v3/athlete/activities?per_page=200&page=${page}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${auth_tokens.access_token}`
+				}
+			}
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				data.forEach((activity) => {
+					activities.push(activity)
+				})
+				if (data.length === 200) {
+					page++
+					getActivities()
+				}
+			})
+			.finally(() => console.log(activities))
+	}
+
 	return (
 		<main>
 			<h1>FitPursuit</h1>
+			<button onClick={getActivities}>Get Activities</button>
 		</main>
 	)
 }
--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	experimental: {
+		appDir: true
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/:path*',
+				destination: 'http://127.0.0.1:5555/:path*' // Proxy to Backend
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
