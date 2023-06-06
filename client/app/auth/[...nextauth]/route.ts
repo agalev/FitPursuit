b01e9374eab2497755683457f53ce21460ae7016
Alexander <alex.galev@yahoo.com>
2023-06-06 09:28:27 -0600

    callback issue7

--- client/app/auth/[...nextauth]/route.ts
+++ client/app/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ const handler = NextAuth({
 			clientId: process.env.STRAVA_CLIENT_ID,
 			clientSecret: process.env.STRAVA_CLIENT_SECRET,
 			authorization:
-				'https://www.strava.com/oauth/authorize?approval_prompt=force&scope=activity:read_all,profile:read_all'
+				'https://www.strava.com/oauth/authorize?redirect_uri=https://fit-pursuit.vercel.app&approval_prompt=force&scope=activity:read_all,profile:read_all'
 		})
 	],
 	callbacks: {
