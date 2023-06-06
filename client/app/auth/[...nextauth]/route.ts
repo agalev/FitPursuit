c89dba8d054006aacd0a2bd97d6241e58d7a4ff9
Alexander <alex.galev@yahoo.com>
2023-06-05 18:05:44 -0600

    callback issue2

--- client/app/auth/[...nextauth]/route.ts
+++ client/app/auth/[...nextauth]/route.ts
@@ -11,13 +11,13 @@ const handler = NextAuth({
 		})
 	],
 	callbacks: {
-		// async redirect({ url, baseUrl }) {
-		// 	// Allows relative callback URLs
-		// 	if (url.startsWith('/')) return `${baseUrl}${url}`
-		// 	// Allows callback URLs on the same origin
-		// 	else if (new URL(url).origin === baseUrl) return url
-		// 	return baseUrl
-		// },
+		async redirect({ url, baseUrl }) {
+			// Allows relative callback URLs
+			if (url.startsWith('/')) return `${baseUrl}${url}`
+			// Allows callback URLs on the same origin
+			else if (new URL(url).origin === baseUrl) return url
+			return baseUrl
+		},
 		async jwt({ token, account }) {
 			// Persist the OAuth access_token to the token right after signin
 			if (account) {
