d6928dfaec9e08cf6b40cc91827a30a148867f36
Alexander <alex.galev@yahoo.com>
2023-06-05 18:16:10 -0600

    callback issue5

--- client/app/auth/[...nextauth]/route.ts
+++ client/app/auth/[...nextauth]/route.ts
@@ -12,9 +12,7 @@ const handler = NextAuth({
 	],
 	callbacks: {
 		async redirect({ url, baseUrl }) {
-			return url.startsWith(baseUrl)
-				? Promise.resolve(url)
-				: Promise.resolve(baseUrl)
+			return Promise.resolve(url)
 		},
 		async jwt({ token, account }) {
 			// Persist the OAuth access_token to the token right after signin
